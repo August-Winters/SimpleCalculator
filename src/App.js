a1734b37bfc2b768c160c74147f60024dfadd57f
unknown <augustus.winters@gmail.com>
2024-05-05 13:19:13 -0400

    first push; sending calculator project to repo

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import {useState, useRef} from "react";
+import "./App.css";
+
+  function App() {
+    const inputRef = useRef(null);
+    const resultRef = useRef(null);
+    const [result, setResult] = useState(0);
+
+    function plus(e) {
+        e.preventDefault();
+        setResult((result) => result + Number(inputRef.current.value));
+      };
+
+    function minus(e) {
+        e.preventDefault();
+        setResult((result) => result - Number(inputRef.current.value));
+    };
+
+    function times(e) {
+        e.preventDefault();
+        setResult((result) => result * Number(inputRef.current.value));
+    };
+
+    function divide(e) {
+        e.preventDefault();
+        setResult((result) => result / Number(inputRef.current.value));
+    };
+
+    function resetInput(e) {
+        e.preventDefault();
+        (inputRef.current.value) = "Type a number";
+    };
+
+    function resetResult(e) {
+        e.preventDefault();
+        setResult((result) => null);
+    };
+
+    return (
+      <div className="App">
+        <div>
+          <h1>Simplest Working Calculator</h1>
+        </div>
+        <form>
+          <p ref={resultRef}>
+            {result}
+          </p>
+          <div className="input">
+            <div>
+                <input
+                    pattern="[0-9]"
+                    ref={inputRef}
+                    type="number"
+                    placeholder="Type a number"
+                    alignSelf="center"
+                />
+            </div>
+        </div>
+          <button onClick={plus}>Add</button>
+          <button onClick={minus}>Subtract</button>
+          <button onClick={times}>Multiply</button>
+          <button onClick={divide}>Divide</button>
+          <button onClick={resetInput}>Reset Input</button>
+          <button onClick={resetResult}>Reset Result</button>
+        </form>
+      </div>
+    );
+  }
+
+  export default App;
